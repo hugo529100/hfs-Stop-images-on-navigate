c87f9b3fa736373840764fbd36b2d23b82137602
Hug3O <131569471+hugo529100@users.noreply.github.com>
2026-09-06 14:33:30 +0800

    Add files via upload

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 1
+exports.version = 1.1
 exports.apiRequired = 8.0
 exports.description = "Stop all image requests when navigating folders - Improves page switching responsiveness and reduces bandwidth usage by interrupting loading of non-essential images"
 exports.repo = "Hug3O/Stop-images-on-navigate"
@@ -9,8 +9,13 @@ exports.frontend_js = "main.js"
 exports.config = {
     whitelist: {
         label: "Whitelist paths (one per line)",
-        type: "text",
-        value: "/images/banner/",
+        type: "string",
+        multiline: true,
+        frontend: true,
+        defaultValue: 
+`
+/images/banner/
+/~/notes/`,
         description: "Images containing these keywords will not be blocked. Enter one keyword per line. By default, only images in the /images/banner/ directory are kept."
     }
 }
